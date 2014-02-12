190cca9b10070ad9392f722ae7362762aad59f36
Patrick Michaud <pmichaud@uw.edu>
2014-02-12 13:33:00 -0800

    Adding stubs

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django-Handlebars-I18N',
+      version='1.0',
+      description='Tools for using django translations in handlebars templates, and making it easier to use translations.',
+     )
